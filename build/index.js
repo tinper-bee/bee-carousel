5af838e8b85f2ef7e6ba702e27c1e3be72dde03d
ahua52 <1468492018@qq.com>
2017-11-24 15:22:38 +0800

    publish 0.0.2

---
+++ build/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _ReactSwiper = require('./ReactSwiper');
+
+var _ReactSwiper2 = _interopRequireDefault(_ReactSwiper);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+exports["default"] = _ReactSwiper2["default"];
+module.exports = exports['default'];
